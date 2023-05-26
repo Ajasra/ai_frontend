59bf6f2c5af6c952b20ab8ee4ced31558921432e
Ajasra <betinvasilij@gmail.com>
2023-05-26 17:22:53 +0800

    - conversation communication with api

--- utils/API/conversarion_api.js
+++ utils/API/conversarion_api.js
@@ -1,65 +1,142 @@
 import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
 
-const conversations_api = process.env.NEXT_PUBLIC_BACKEND_URL
-const api_key = process.env.NEXT_PUBLIC_API_KEY
+const conversations_api = process.env.NEXT_PUBLIC_BACKEND_URL;
+const api_key = process.env.NEXT_PUBLIC_API_KEY;
 
 export async function getApiResponse(prompt, user_id, document_id, conv_id) {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+  const raw = JSON.stringify({
+    user_message: prompt,
+    document: document_id,
+    conversation_id: conv_id,
+    user_id: user_id,
+    api_key: api_key,
+  });
 
-    const raw = JSON.stringify({
-        "user_message": prompt,
-        "document": document_id,
-        "conversation_id": conv_id,
-        "user_id": user_id,
-        "api_key": api_key
-    });
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
 
-    const requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow',
-    };
+  const api_url = `${conversations_api}/conv/get_response_doc`;
 
-    const api_url = `${conversations_api}/conv/get_response_doc`
-
-    try{
-        const response = await fetch(api_url, requestOptions)
-        return await response.json()
-    }catch (error){
-        console.log(error)
-        return error
-    }
-
+  try {
+    const response = await fetch(api_url, requestOptions);
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 }
 
 export async function getConversationHistory(conv_id) {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
+  const raw = JSON.stringify({
+    conv_id: conv_id,
+    limit: 100,
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  const api_url = `${conversations_api}/conv/get_history`;
+
+  try {
+    const response = await fetch(api_url, requestOptions);
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+}
+
+export async function getConversationByIdApi(conv_id) {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    conv_id: conv_id,
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  const api_url = `${conversations_api}/conv/get_selected_conv`;
+
+  try {
+    const response = await fetch(api_url, requestOptions);
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+}
+
+
+export async function updateConversationAPI(conv_id, title) {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    conv_id: conv_id,
+    title: title,
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  const api_url = `${conversations_api}/conv/update`;
+
+  try {
+    const response = await fetch(api_url, requestOptions);
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+}
+
+
+export async function deleteConversationAPI(conv_id) {
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
     const raw = JSON.stringify({
-        "conv_id": conv_id,
-        "limit": 100,
+        conv_id: conv_id,
     });
 
     const requestOptions = {
-        method: 'POST',
+        method: "POST",
         headers: myHeaders,
         body: raw,
-        redirect: 'follow',
+        redirect: "follow",
     };
 
-    const api_url = `${conversations_api}/conv/get_history`
+    const api_url = `${conversations_api}/conv/delete`;
 
-    try{
-        const response = await fetch(api_url, requestOptions)
-        return await response.json()
-    }catch (error){
-        console.error(error)
-        return error
+    try {
+        const response = await fetch(api_url, requestOptions);
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        return error;
     }
-
 }
